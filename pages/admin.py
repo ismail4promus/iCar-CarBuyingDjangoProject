c4eb130c20c700713f00f192289db7ab01a25347
ismail4promus <ismail4promus@gmail.com>
2022-04-20 17:48:05 +0600

    after team admin customization

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Team
+from django.utils.html import format_html
 # Register your models here.
-admin.site.register(Team)
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src = "{}" width = "100" style = "border-radius: 50px" />'.format(object.photo.url))
+
+    thumbnail.short_description = 'photo'
+
+    list_display = ('id','thumbnail', 'firstName', 'designation', 'created_date')
+    list_display_links = ('id','firstName','thumbnail',)
+    search_fields = ('firstName', 'lastName', 'designation')
+    list_filter = ('designation',)
+
+admin.site.register(Team, TeamAdmin)
